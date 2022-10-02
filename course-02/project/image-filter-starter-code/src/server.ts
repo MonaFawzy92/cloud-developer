5fcf811cea8861d73fecb67fc2728ccc6bac98a5
MonaFawzy92 <58727333+MonaFawzy92@users.noreply.github.com>
2022-10-02 11:27:50 +0200

    add new endPoint

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,17 +30,31 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get("/filteredimage?image_url={{URL}}", async (req, res) => {
+    let { image_url } = req.query.image_url;
+    if (!image_url)
+      return res.status(400).send("image_url  is required for this request");
+
+    let image = await filterImageFromURL(image_url);
+    if (!image)
+      return res.status(400).send("couldn't find the image");
+
+    return res.status(200).sendFile(image);
+    // return res.sendFile(file, function () {
+    //   deleteLocalFiles([file]);
+    // });
+  });
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
